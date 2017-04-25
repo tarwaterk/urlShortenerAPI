d756b02ccb31e65d9294f074ae58a7b3b3eed5c5
Kevin Tarwater <tarwaterk@gmail.com>
2017-04-25 12:19:52 -0500

    creates new route for redirecting shortURLs and starts debuggin async issue inside the function that creates the shortURLs

--- index.js
+++ index.js
@@ -16,29 +16,40 @@ var createShortURL = function(longURL) {
 
 		collection.insert({"shortURL" : shortURL,
 							"longURL": longURL}, function(err, result) {
-								console.log(result);
-								dbEntry = result;
-							})
-
+								//console.log(result.ops);
+								dbEntry = result.ops;
+								console.log(dbEntry);
+							});
+		
 		db.close();
 	});
 
 	return dbEntry;
 };
 
-/*
-app.get("/:protocol//:subdomain.:domain.:extension", function(req, res) {
-	*/
+var redirect = function(shortUrlSlug) {
+
+} 
+
 app.get("/:protocol//:longURL([a-zA-Z\.\-\/]{0,})", function(req, res) {
 	console.log(req.params);
 	var dbEntry = {};
-	var isURL = false; //needs to be completed
+	var isURL = true; //needs to be completed
 	if(isURL) {
-		dbEntry = createShortURL(req.params.longURL);
+		dbEntry = createShortURL(req.params.protocol + "//" + req.params.longURL);
 	}
 	res.send(dbEntry);
+});
+
+app.get("/:shortUrlSlug([0-9]{4})", function(req, res) {
+	console.log(req.params.shortUrlSlug);
+
+	res.send("redirect should have been called");
 });
 
 app.listen(port, function(req, res) {
 	console.log("App started at http://localhost:" + port);
 });
+
+// Need to find out how to hold off on returning from functions until the database opertions
+// are finished
